9c7732e2208b34883440a76e64ebff8c3868bc54
thibaud <thibaud@BAT-PC.home>
2021-12-28 12:45:57 +0100

    rework on max

--- src/main/scala/scala/functional/programming/chapter3/NumberTree.scala
+++ src/main/scala/scala/functional/programming/chapter3/NumberTree.scala
@@ -1,10 +1,9 @@
 package scala.functional.programming.chapter3
 
+import scala.functional.programming.chapter3.Tree._
+
 object NumberTree {
   def max(tree: Tree[Int]): Int = {
-    tree match {
-      case Branch(left, right) => max(left) max max(right)
-      case Leaf(value) => value
-    }
+    fold(tree)(a => a)((a: Int, b: Int) => a max b)
   }
 }
