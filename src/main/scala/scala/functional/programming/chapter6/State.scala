13f9f8c0013c2c65f98cf2381f8700798d9ee2b9
TLevasseur <you@example.com>
2022-01-18 17:47:21 +0100

    6.10

---
+++ src/main/scala/scala/functional/programming/chapter6/State.scala
@@ -0,0 +1,27 @@
+package scala.functional.programming.chapter6
+
+import scala.functional.programming.chapter6.State.unit
+
+case class State[S, +A](run: S => (A, S)) {
+  def map[B](f: A => B): State[S, B] = {
+    State(s => {
+      val (a, s1) = run(s)
+      (f(a), s1)
+    })
+  }
+
+  def map2[B, C](rb: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap(a => rb.flatMap(b => unit(f(a, b))))
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+    val (a, s1) = run(s)
+    f(a).run(s1)
+  })
+}
+
+object State {
+
+  def unit[S, A](a: A): State[S, A] = State(s => {
+    (a, s)
+  })
+}
