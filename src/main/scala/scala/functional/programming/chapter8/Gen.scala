df593ece3eca79f16f05df78c318b30065619e6f
TLevasseur <you@example.com>
2022-02-06 20:58:43 +0100

    8.7 & 8.8

--- src/main/scala/scala/functional/programming/chapter8/Gen.scala
+++ src/main/scala/scala/functional/programming/chapter8/Gen.scala
@@ -2,21 +2,33 @@ package scala.functional.programming.chapter8
 
 import scala.functional.programming.chapter6.{RNG, State}
 
+case class Gen[A](sample: State[RNG, A]) {
+
+  def flatmap[B](f: A => Gen[B]): Gen[B] = f(this.sample)
+
+  def listOfN(size: Gen[Int]): Gen[List[A]] = size flatmap (e => Gen.listOfN(e, this))
+}
+
+trait Prop {
+  type SuccessCount = Int
+  type FailedCase = String
+
+  def check(): Either[(FailedCase, SuccessCount), SuccessCount]
+
+  def &&(p: Prop): Prop = {
+    ???
+  }
+}
+
 object Gen {
 
-  trait Prop {
-    type SuccessCount = Int
-    type FailedCase = String
-
-    def check(): Either[(FailedCase, SuccessCount), SuccessCount]
-
-    def &&(p: Prop): Prop = {
-      ???
-    }
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = {
+    boolean.flatmap(if (_) g1 else g2)
   }
 
-
-  case class Gen[A](sample: State[RNG, A])
+  def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] = {
+    Gen(State(RNG.double)).flatmap(rnd => if (rnd > (g1._2 / (g1._2 + g2._2))) g2._1 else g1._1)
+  }
 
   def unit[A](a: => A): Gen[A] = Gen(State.unit(a))
 
@@ -31,10 +43,6 @@ object Gen {
   }
 
   def listOf[A](a: Gen[A]): Gen[List[A]] = {
-    ???
-  }
-
-  def listOfN[A](n: Int, a: Gen[A]): Gen[List[A]] = {
     ???
   }
 
