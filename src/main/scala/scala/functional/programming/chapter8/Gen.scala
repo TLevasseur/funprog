6b87c03e47ffdfeb320fefc3fb2033f7507e836c
thibaud <thibaud@BAT-PC.home>
2022-09-07 18:10:08 +0200

    8.5

--- src/main/scala/scala/functional/programming/chapter8/Gen.scala
+++ src/main/scala/scala/functional/programming/chapter8/Gen.scala
@@ -1,5 +1,8 @@
 package scala.functional.programming.chapter8
 
+import scala.annotation.tailrec
+import scala.functional.programming.chapter6.{RNG, State}
+
 
 case class Prop(check: Boolean) {
   def &&(other: Prop): Prop = {
@@ -12,10 +15,32 @@ object Prop {
 }
 
 object Gen {
-  def unit[A](a: => A): Gen[A] = ???
+  def unit[A](a: => A): Gen[A] = Gen(State.unit(a))
+
+  def choose(start: Int, stopExclusive: Int): Gen[Int] = {
+    Gen(State(RNG.double).map(start + _ * (stopExclusive - start)).map(_.toInt))
+  }
+
+  def boolean: Gen[Boolean] = {
+    Gen(State(RNG.boolean))
+  }
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = {
+    @tailrec
+    def helper(l: List[A], n: Int, g: Gen[A])(rng: RNG): (List[A], RNG) = {
+      if (n == 0)
+        (l, rng)
+      else {
+        val (x, newGen) = g.sample.run(rng)
+        helper(l :+ x, n - 1, g)(newGen)
+      }
+    }
+
+    Gen(State(helper(List(), n, g)))
+  }
 }
 
-trait Gen[A] {
+case class Gen[+A](sample: State[RNG, A]) {
   def map[A, B](f: A => B): Gen[B] = ???
 
   def flatMap[A, B](f: A => Gen[B]): Gen[B] = ???
--- src/test/scala/scala/functional/programming/chapter8/GenTest.scala
+++ src/test/scala/scala/functional/programming/chapter8/GenTest.scala
@@ -1,7 +1,9 @@
 package scala.functional.programming.chapter8
 
-import org.junit.Assert.assertEquals
+import org.junit.Assert.{assertEquals, assertTrue}
 import org.junit.{Assert, Test}
+
+import scala.functional.programming.chapter6.SimpleRNG
 class GenTest {
 
   @Test
@@ -10,5 +12,12 @@ class GenTest {
     Assert.assertFalse((Prop(false) && Prop(true)).check)
     Assert.assertFalse((Prop(true) && Prop(false)).check)
     Assert.assertFalse((Prop(false) && Prop(false)).check)
+  }
+
+  @Test
+  def testChoose() : Unit = {
+    val (x, _) = Gen.choose(10,20).sample.run(SimpleRNG(12345678987654321L))
+    assertTrue(x<20)
+    assertTrue(x>=10)
   }
 }
